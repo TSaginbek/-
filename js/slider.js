5323a98197204e1f0d1abd3cf72f4fcfefccebfd
Tungatarov Saginbek <tungatarovs03@mail.ru>
2021-02-21 16:43:24 +0500

    HTML, CSS, JS, Jquery

---
+++ js/slider.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('.slider').slick({
+        arrows:false,
+        dots:true,
+        adaptiveHeight:true,
+        slidesToShow:1,
+        slidesToScroll:1,
+        speed:500,
+        easing:'ease',
+        infinite:true,
+        autoplay:false,
+        autoplaySpeed:4000,
+        
+    });
+});
